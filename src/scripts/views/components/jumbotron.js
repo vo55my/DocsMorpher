9bdbdcdd99975bce0bbe0516fd0b7971d5fdd046
Dhaifullah HIlmy <dhaifullahhilmy@gmail.com>
2023-06-11 16:10:24 +0700

    Fix Bug Part 2

--- src/scripts/views/components/jumbotron.js
+++ src/scripts/views/components/jumbotron.js
@@ -5,7 +5,7 @@ class Jumbotron extends HTMLElement {
 
   render() {
     this.innerHTML = `
-    <section class="jumbotron py-5 d-flex">
+    <section class="jumbotron py-5 my-5 d-flex">
       <div class="container text-center py-5 my-5 col-lg-8 text-center align-items-center justify-content-center">
         <h1 class="display-1 fw-bold" id="judul">Welcome to DocsMorpher</h1>
         <p class="fs-5 pt-3">Konversi dokumen Word, Excel, Powerpoint, dan Gambar ke PDF yang dapat diakses secara online.</p>
