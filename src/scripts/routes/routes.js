af9bab6aeefdcdd2836a11661db6344409f0f285
Dhaifullah HIlmy <dhaifullahhilmy@gmail.com>
2023-06-18 20:56:25 +0700

    Update 2

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,6 +1,5 @@
 import Home from '../views/pages/home';
 import About from '../views/pages/about';
-import PdfImg from '../views/pages/pdf-img';
 import ExcelPdf from '../views/pages/excel-pdf';
 import ImgPdf from '../views/pages/img-pdf';
 import PowerpointPdf from '../views/pages/powerpoint-pdf';
@@ -10,7 +9,6 @@ const routes = {
   '/': Home, // default page
   '/home': Home,
   '/about': About,
-  '/pdf-img': PdfImg,
   '/excel-pdf': ExcelPdf,
   '/img-pdf': ImgPdf,
   '/powerpoint-pdf': PowerpointPdf,
--- src/scripts/views/components/header.js
+++ src/scripts/views/components/header.js
@@ -33,7 +33,6 @@ class Header extends HTMLElement {
                   <li><a class="dropdown-item" href="#/powerpoint-pdf"><img data-src="/icons/LOGO PPT PDF.webp" alt="Powerpoint to PDF" class="w-25 lazyload"> PPT to PDF</a></li>
                   <li><a class="dropdown-item" href="#/excel-pdf"><img data-src="/icons/LOGO XSL PDF.webp" alt="Excel to PDF" class="w-25 lazyload"> Excel to PDF</a></li>
                   <li><a class="dropdown-item" href="#/img-pdf"><img data-src="/icons/LOGO IMG PDF.webp" alt="IMG to PDF" class="w-25 lazyload"> IMG to PDF</a></li>
-                  <li><a class="dropdown-item" href="#/pdf-img"><img data-src="/icons/LOGO PDF IMG.webp" alt="PDF to IMG" class="w-25 lazyload"> PDF to IMG</a></li>
                 </ul>
               </li>
               <li class="nav-item">
